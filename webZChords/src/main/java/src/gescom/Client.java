7c02fccd6d3d88ef8f5e763b9ba02c4e08af0766
legoeland06 <45659378+legoeland06@users.noreply.github.com>
2022-06-28 15:38:25 +0200

    Delete webZChords/src/main/java/src/gescom directory

--- webZChords/src/main/java/src/gescom/Client.java
+++
@@ -1,9 +0,0 @@
-package src.gescom;
-
-import java.util.ArrayList;
-
-public interface Client {
-
-	public  ArrayList<String> chercher(String client);
-	public void validerCommande(String client);
-}
